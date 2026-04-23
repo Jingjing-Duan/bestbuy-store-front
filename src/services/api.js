d9fa5a92005e88d4a48f39e5fb7da19a0a554cb1
Jingjing Duan <duan0029@algonquinlive.com>
2026-04-23 09:08:18 -0400

    fix nginx.conf

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const productApi = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: '/api/products'
 });
 
 export const orderApi = axios.create({
-  baseURL: 'http://localhost:3002'
+  baseURL: '/api/orders'
 });
